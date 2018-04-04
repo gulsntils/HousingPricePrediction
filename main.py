0bd54f0d0e325db6c73e6734f377975b833d0ebf
Suyash <suyash.srivastava96@gmail.com>
2018-04-05 00:03:40 +0530

    Handle missing values using Imputation

--- main.py
+++ main.py
@@ -5,22 +5,48 @@ from sklearn.tree import DecisionTreeRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
+from sklearn.preprocessing import Imputer
 
 # training set
 TRAIN_FILE_PATH = "train.csv"
 # test set
 TEST_FILE_PATH = "test.csv"
-# sample columns
-COLUMNS_OF_INTEREST = ['LotArea', 'YearBuilt', '1stFlrSF', '2ndFlrSF', 'FullBath',
-			'BedroomAbvGr', 'TotRmsAbvGrd']
+# numeric columns
+numeric_cols = []
+
+# Utility function to get imputet data
+def get_imputed_data(X_train, X_test):
+	# get columns with missing values
+	cols_with_missing = [col for col in X_train.columns 
+								if X_train[col].isnull().any()]
+
+	# add a label for every column that is missing
+	for col in cols_with_missing:
+		X_train[col + '_was_missing'] = X_train[col].isnull()
+		X_test[col + '_was_missing'] = X_test[col].isnull()
+
+	# impute data
+	imputer = Imputer()
+	imputed_X_train = imputer.fit_transform(X_train)
+	imputed_X_test = imputer.transform(X_test)
+
+	return imputed_X_train, imputed_X_test
 
 # get training data from csv file
 def get_training_data():
 	training_data = pd.read_csv(TRAIN_FILE_PATH)
 	# select target
 	y = training_data.SalePrice
-	# select sample columns
-	X = training_data[COLUMNS_OF_INTEREST]
+
+	global numeric_cols
+
+	# select numeric columns
+	numeric_cols = [col for col in training_data.columns
+		if training_data.dtypes[col] != 'object']
+	numeric_cols.remove('SalePrice')
+
+	# select numeric data
+	X = training_data[numeric_cols]
 
 	return y, X
 
@@ -28,7 +54,10 @@ def get_training_data():
 def get_test_data():
 	test_data = pd.read_csv(TEST_FILE_PATH)
 
-	X = test_data[COLUMNS_OF_INTEREST]
+	test_cols = [col for col in test_data.columns
+		if test_data.dtypes[col] != 'object']
+
+	X = test_data[numeric_cols]
 
 	return X, test_data
 
@@ -70,6 +99,9 @@ def build_prediction_file(predictions, test_data):
 y_train, X_train = get_training_data()
 # extract test set
 X_test, test_data = get_test_data()
+
+# get imputed data
+X_train, X_test = get_imputed_data(X_train, X_test)
 
 # test model on training set
 error = test_on_train_set(y_train, X_train)
